97505c92fc2bcc69b544624dfe7dee28ea6588eb
Alex D <alexanderdelatour1@gmail.com>
2024-09-25 15:15:39 -0400

    PoissonDisc created

---
+++ Assets/Avoider.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Avoider : MonoBehaviour
+{
+    [SerializeField]
+    Transform targetToAvoid;
+    [SerializeField]
+    float distanceWhenToAvoid;
+    [SerializeField]
+    float poissonDiscHeight, poissonDiscWidth;
+    List<Vector3> localPoissonPoints=new List<Vector3>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoissonGenerator();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (LineOfSightCheck(this.transform.position))
+        {
+
+            //var points= localPoissonPoints.ToList().Select(e=>LineOfSightCheck(this.transform.TransformPoint(e)));
+            foreach (var point in localPoissonPoints)
+            {
+               
+                if (!LineOfSightCheck(this.transform.TransformPoint(point)))
+                {
+                    Debug.DrawLine(this.transform.position, this.transform.TransformPoint(point), Color.green);
+                }
+                else
+                {
+                    Debug.DrawLine(this.transform.position, this.transform.TransformPoint(point), Color.red);
+                }
+               
+            }
+
+
+        }
+        
+    }
+
+    bool LineOfSightCheck(Vector3 startPosition)
+    {
+        Vector3 direction = (targetToAvoid.position - startPosition);
+        float distance = direction.magnitude;
+        direction = direction.normalized;
+        Ray ray = new Ray(startPosition, direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, distanceWhenToAvoid) && hit.collider.transform == targetToAvoid)
+        {
+
+            return true;
+        }
+        return false;
+    }
+
+    void PoissonGenerator()
+    {
+        PoissonDiscSampler poissonDisc = new PoissonDiscSampler(poissonDiscWidth, poissonDiscHeight, 3);
+        foreach(var point in poissonDisc.Samples())
+        {
+            Vector3 newPoint = new Vector3(point.x - poissonDiscWidth/2, 0, point.y - poissonDiscHeight/2);
+               
+            localPoissonPoints.Add(newPoint);
+            
+          
+        }
+
+
+    }
+
+
+}
