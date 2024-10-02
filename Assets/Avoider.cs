be1226ba5d8c87f4b541659bcc822befaad1c09b
Dylan Torres <glaciershield@gmail.com>
2024-10-02 14:18:46 -0400

    Avoider Now Requires NavMeshAgent
    The Avoider script now requires a NavMeshAgent component. If the player tries to remove the NavMeshAgent off of the Avoider prefab, the script will cancel this because of its dependency.

--- Assets/Avoider.cs
+++ Assets/Avoider.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Avoider : MonoBehaviour
 {
     [SerializeField]
